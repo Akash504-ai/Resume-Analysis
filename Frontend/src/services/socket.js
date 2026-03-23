05278db6d61cba8e29e63683e18a0715fb6ca39a
Akash Santra <santraakash999@gmail.com>
2026-03-23 08:59:13 +0530

    socket rout updt

--- Frontend/src/services/socket.js
+++ Frontend/src/services/socket.js
@@ -1,10 +1,10 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "http://localhost:3000";
+const SOCKET_URL = import.meta.env.VITE_BACKEND_URL;
 
 const socket = io(SOCKET_URL, {
   withCredentials: true,
-  autoConnect: false
+  autoConnect: false,
 });
 
 export default socket;
