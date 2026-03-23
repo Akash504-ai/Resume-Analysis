6b353d4d368d38a015c4483042d6d8fd90aea8a6
Akash Santra <santraakash999@gmail.com>
2026-03-23 09:26:31 +0530

    plan uri updt

--- Frontend/src/features/plan/services/plan.api.js
+++ Frontend/src/features/plan/services/plan.api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:3000/api/plans";
+const API = `${import.meta.env.VITE_BACKEND_URL}/api/plans`;
 
 export const createPlan = async (data) => {
   const res = await axios.post(API, data, { withCredentials: true });
